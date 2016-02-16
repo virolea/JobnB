8469f5f8755e9ceafcb763de410604ef1df3836b
Vincent Rolea <vincent.rolea@gmail.com>
2016-02-16 14:58:21 +0100

    corrected facebook implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'omniauth_callbacks_controller/faceebook'
-
+  get 'omniauth_callbacks_controller/facebook'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
-
   resources :posts, only: [:index, :show, :new, :create, :delete]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
