83fb1b3855b4bc518af82a02821e333987072081
Vincent Rolea <vincent.rolea@gmail.com>
2016-02-16 11:25:15 +0100

    added nested routes for mission inside posts

---
+++ app/controllers/missions_controller.rb
@@ -0,0 +1,6 @@
+class MissionsController < ApplicationController
+  def create
+    mission = Mission.new
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :posts, only: [:index, :show, :new, :create, :delete]
+  resources :posts, only: [:index, :show, :new, :create, :delete] do
+    resources :missions, only: [:create]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
