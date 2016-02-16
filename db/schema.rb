853751cd7c1bee4747d8a4e3dd69af3840d39ae2
Charles Valeyre <cvaleyre@gmail.com>
2016-02-16 14:35:57 +0100

    Facebook2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216104847) do
+ActiveRecord::Schema.define(version: 20160216113312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 20160216104847) do
     t.string   "last_name"
     t.string   "token"
     t.datetime "token_expiry"
-    t.string   "employer"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
