4e813db880b3a33f7943466abaca6d2c553b5ca2
Vincent Rolea <vincent.rolea@gmail.com>
2016-02-16 11:16:52 +0100

    added relations to user and mission

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :purchases, :class_name => 'Mission', :foreign_key => 'buyer_user_id'
+  has_many :sales, :class_name => 'Mission', :foreign_key => 'seller_user_id'
 end
