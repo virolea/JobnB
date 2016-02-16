2c6b188b549c2e5735582dd7879c3a58cc7afa36
Vincent Rolea <vincent.rolea@gmail.com>
2016-02-16 21:40:47 +0100

    changed migration scheme

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160216104707) do
-=======
 ActiveRecord::Schema.define(version: 20160216171305) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,9 +66,6 @@ ActiveRecord::Schema.define(version: 20160216171305) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-<<<<<<< HEAD
   add_foreign_key "missions", "posts"
-=======
->>>>>>> master
   add_foreign_key "posts", "users"
 end
