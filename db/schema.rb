7e807a6bcfed506cefc2c8434779b157cef09bad
Vincent Rolea <vincent.rolea@gmail.com>
2016-02-16 10:57:41 +0100

    created Mission model and database migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215152308) do
+ActiveRecord::Schema.define(version: 20160216095526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "missions", force: :cascade do |t|
+    t.string   "status"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "selling_user_id"
+    t.integer  "buying_user_id"
+    t.integer  "post_id"
+  end
+
+  add_index "missions", ["post_id"], name: "index_missions_on_post_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -45,4 +56,5 @@ ActiveRecord::Schema.define(version: 20160215152308) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "missions", "posts"
 end
