03299d556e83f9b76511f7ac139693ca57ef5648
Vincent Rolea <vincent.rolea@gmail.com>
2016-02-16 11:41:47 +0100

    added mission controller

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,6 +1,10 @@
 class MissionsController < ApplicationController
   def create
     mission = Mission.new
-
+    mission.buying_user = current_user
+    mission.selling_user = Post.find(params[:post_id]).user
+    mission.post = Post.find(params[:post_id])
+    mission.status = "pending"
+    mission.save
   end
 end
--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,4 +1,4 @@
 class Mission < ActiveRecord::Base
-  belongs_to :buying_user, :class_name => 'User'
-  belongs_to :selling_user, :class_name => 'User'
+  belongs_to :buying_user, class_name: 'User'
+  belongs_to :selling_user, class_name: 'User'
 end
