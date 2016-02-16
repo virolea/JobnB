305ff50246433541a4a35c9f712a2e4d1634a06b
Vincent Rolea <vincent.rolea@gmail.com>
2016-02-16 11:53:10 +0100

    added validations to missions

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,4 +1,6 @@
 class Mission < ActiveRecord::Base
   belongs_to :buying_user, class_name: 'User'
   belongs_to :selling_user, class_name: 'User'
+
+  validates :status, :selling_user_id, :buying_user_id, :post_id, presence: true
 end
