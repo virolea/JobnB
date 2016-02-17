1058fe26abb5555797d15280289f3135e7864326
Vincent Rolea <vincent.rolea@gmail.com>
2016-02-17 11:06:45 +0100

    terminated backend for missions, verfied, it works

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,21 +1,28 @@
 class MissionsController < ApplicationController
-  before_action :set_post, only: [:create]
+  before_action :set_post, only: [:new, :create]
 
   def new
+    @mission = Mission.new
   end
 
   def create
-    @mission = Mission.new
+    @mission = Mission.new(mission_params)
     @mission.buying_user = current_user
     @mission.selling_user = @post.user
     @mission.post = @post
     @mission.status = "pending"
     @mission.save
+
+    redirect_to post_path(@post)
   end
 
   private
 
   def set_post
     @post = Post.find(params[:post_id])
+  end
+
+  def mission_params
+    params.require(:mission).permit(:description)
   end
 end
--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,6 +1,7 @@
 class Mission < ActiveRecord::Base
   belongs_to :buying_user, class_name: 'User'
   belongs_to :selling_user, class_name: 'User'
+  belongs_to :post
 
   validates :status, :selling_user_id, :buying_user_id, :post_id, :description, presence: true
 end
