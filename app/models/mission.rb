a8e3aa90a8c678024597792edcbbee76e4bff1bd
Vincent Rolea <vincent.rolea@gmail.com>
2016-02-17 10:55:24 +0100

    added missions views and validations to mission.rb

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -2,5 +2,5 @@ class Mission < ActiveRecord::Base
   belongs_to :buying_user, class_name: 'User'
   belongs_to :selling_user, class_name: 'User'
 
-  validates :status, :selling_user_id, :buying_user_id, :post_id, presence: true
+  validates :status, :selling_user_id, :buying_user_id, :post_id, :description, presence: true
 end
