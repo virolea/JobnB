d69bf6ca59666ea72d622542d2d781060d46392f
Vincent Rolea <vincent.rolea@gmail.com>
2016-02-18 19:03:03 +0100

    deploy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,6 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require bootstrap-datepicker
-//= require jquery-smooth-scroll
 //= require underscore
 //= require gmaps/google
 //= require_tree .
