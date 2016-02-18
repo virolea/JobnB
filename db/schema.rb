e33bd23e55ffdd5a9b789cd16e27385847733a82
Vincent Rolea <vincent.rolea@gmail.com>
2016-02-18 12:24:04 +0100

    changed card style on home page

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160217153049) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,8 +38,8 @@ ActiveRecord::Schema.define(version: 20160217153049) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "user_id"
-    t.string   "address"
     t.string   "photo"
+    t.string   "address"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
