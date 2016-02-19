629042c2e6d57fe6eb7d13c21c4a64bc9d90e651
Vincent Rolea <vincent.rolea@gmail.com>
2016-02-19 15:20:13 +0100

    migrations done

--- db/schema.rb
+++ db/schema.rb
@@ -38,8 +38,8 @@ ActiveRecord::Schema.define(version: 20160217153049) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "user_id"
-    t.string   "address"
     t.string   "photo"
+    t.string   "address"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
